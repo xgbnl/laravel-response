cb58e919f7fe3a7bb3fd619381de751c75b251de
xgbnl <745502274@qq.com>
2026-04-07 21:22:12 +0800

    feat: 恢复自动注册&doc: 排除中间件

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Elephant\Response;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Routing\Router;
 
 final class ServiceProvider extends BaseServiceProvider
 {
-	public function register(): void
-	{
-		$this->app->bind(Reportable::class, JsonReport::class);
-		$this->app->alias(ThrowableReport::class, 'Reportable');
-	}
+    public function register(): void
+    {
+        $this->app->bind(Reportable::class, JsonReport::class);
+        $this->app->alias(ThrowableReport::class, 'Reportable');
+    }
+
+    public function boot(Router $router): void
+    {
+        $router->aliasMiddleware('body.advice', FormatResponseBodyAdvice::class)
+            ->pushMiddlewareToGroup('api', 'body.advice');
+    }
 }
