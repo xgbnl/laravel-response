36947b767c44b74c691f4763bea933b3c596cf4b
xgbnl <745502274@qq.com>
2026-04-09 22:53:45 +0800

    feat: 新增异常

--- src/ThrowableReport.php
+++ src/ThrowableReport.php
@@ -15,56 +15,60 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use BadMethodCallException;
 use Throwable;
 
 readonly class ThrowableReport implements Reportable
 {
-	public function __construct(private Application $app) {}
+    public function __construct(private Application $app)
+    {
+    }
 
-	public function report(Response|Throwable $response): array
-	{
-		$throwable = $this->wrap($response);
+    public function report(Response|Throwable $response): array
+    {
+        $throwable = $this->wrap($response);
 
-		$struct = [
-			'msg' 	=> $throwable->getMessage(),
-			'code'  => $throwable->getCode(),
-		];
+        $struct = [
+            'msg' => $throwable->getMessage(),
+            'code' => $throwable->getCode(),
+        ];
 
-		if ($this->renderThrowableTrace() && $throwable->getCode() === 500) {
-			return array_merge($struct, [
-				'exception' => get_class($throwable->getPrevious()),
-				'file'      => $throwable->getFile(),
-				'line'      => $throwable->getLine(),
-				'trace'     => Collection::make($throwable->getPrevious()->getTrace())
-					->map(fn(array $trace): array => Arr::except($trace, ['args', 'type'])),
-			]);
-		}
+        if ($this->renderThrowableTrace() && $throwable->getCode() === 500) {
+            return array_merge($struct, [
+                'exception' => get_class($throwable->getPrevious()),
+                'file' => $throwable->getFile(),
+                'line' => $throwable->getLine(),
+                'trace' => Collection::make($throwable->getPrevious()->getTrace())
+                    ->map(fn(array $trace): array => Arr::except($trace, ['args', 'type'])),
+            ]);
+        }
 
-		return array_merge($struct, ['data' => null]);
-	}
+        return array_merge($struct, ['data' => null]);
+    }
 
-	protected function wrap(Throwable $throwable): Throwable
-	{
+    protected function wrap(Throwable $throwable): Throwable
+    {
 
-		if ($throwable instanceof QueryException) {
-			return new Exception($throwable->getMessage(), 500, $throwable);
-		}
+        if ($throwable instanceof QueryException) {
+            return new Exception($throwable->getMessage(), 500, $throwable);
+        }
 
-		$errors = match (true) {
-			$throwable instanceof MissingAbilityException                                               => [$throwable->getMessage(), 403],
-			$throwable instanceof AuthenticationException                                               => [$throwable->getMessage(), 401],
-			$throwable instanceof MethodNotAllowedException                                             => [$throwable->getMessage(), 405],
-			$throwable instanceof ValidationException                                                   => [$throwable->validator->errors()->first(), 422],
-			$throwable instanceof HttpException                                                         => [$throwable->getMessage(), $throwable->getStatusCode()],
-			$throwable instanceof ModelNotFoundException || $throwable instanceof NotFoundHttpException => [$throwable->getMessage(), 404],
-			default                                                                                     => [$throwable->getMessage(), $throwable->getCode() >= 500 || $throwable->getCode() === 0 ? 500 : $throwable->getCode()]
-		};
+        $errors = match (true) {
+            $throwable instanceof MissingAbilityException => [$throwable->getMessage(), 403],
+            $throwable instanceof AuthenticationException => [$throwable->getMessage(), 401],
+            $throwable instanceof MethodNotAllowedException => [$throwable->getMessage(), 405],
+            $throwable instanceof ValidationException => [$throwable->validator->errors()->first(), 422],
+            $throwable instanceof HttpException => [$throwable->getMessage(), $throwable->getStatusCode()],
+            $throwable instanceof ModelNotFoundException || $throwable instanceof NotFoundHttpException => [$throwable->getMessage(), 404],
+            $throwable instanceof BadMethodCallException => [$throwable->getMessage(), 500],
+            default => [$throwable->getMessage(), $throwable->getCode() >= 500 || $throwable->getCode() === 0 ? 500 : $throwable->getCode()]
+        };
 
-		return new Exception(current($errors), next($errors), $throwable);
-	}
+        return new Exception(current($errors), next($errors), $throwable);
+    }
 
-	protected function renderThrowableTrace(): bool
-	{
-		return method_exists($this->app, 'isLocal') && $this->app->isLocal() && $this->app->get('config')->get('app.debug');
-	}
+    protected function renderThrowableTrace(): bool
+    {
+        return method_exists($this->app, 'isLocal') && $this->app->isLocal() && $this->app->get('config')->get('app.debug');
+    }
 }
